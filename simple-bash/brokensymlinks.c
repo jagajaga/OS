f3bb2bbe44b36015e9a2085057a6a1127fa3180c
jagajaga <senia00@gmail.com>
2013-04-12 02:10:39 +0400

    infinite loop

--- simple-bash/brokensymlinks.c
+++ simple-bash/brokensymlinks.c
@@ -2,16 +2,19 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <dirent.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
 
 int get_int(char * c)
 {
-int result = 0;
-int i = 0;
-for (i = 0; c[i] != 0; i++)
-{
-    result = (result * 10) + (int) (c[i] - '0');
-}
-return result;
+    int result = 0;
+    int i = 0;
+    for (i = 0; c[i] != 0; i++)
+    {
+        result = (result * 10) + (int) (c[i] - '0');
+    }
+    return result;
 }
 
 void brokensymlinks(char * name)
@@ -19,46 +22,34 @@ void brokensymlinks(char * name)
     DIR * d;
     struct dirent * dir;
     struct stat sbuf;
+    if (name == ".")
+    {
+        return;
+    }
     d = opendir(name);
     if (d)
     {
         while ((dir = readdir(d)) != NULL)
         {
+            printf("%s\n", dir->d_name);
             lstat(dir->d_name, &sbuf);
-            if (!access(dir->d_name, F_OK) && S_ISLNK(sbuf.st_mode))
+            if (S_ISDIR(sbuf.st_mode))
             {
-                printf("%s\n", s->d_name);
+                brokensymlinks(dir->d_name);
             }
-                }
-            closedir(d);
+            if (access(dir->d_name, F_OK) && S_ISLNK(sbuf.st_mode))
+            {
+                printf("%s\n", dir->d_name);
+            }
         }
-        struct stat stat;
-        if(
-            for (
+        closedir(d);
+    }
+}
 
-                }
-
-/* int main(void) */
-/* { */
-/*   DIR           *d; */
-/*   struct dirent *dir; */
-/*   d = opendir("."); */
-/*   if (d) */
-/*   { */
-/*     while ((dir = readdir(d)) != NULL) */
-/*     { */
-/*       printf("%s\n", dir->d_name); */
-/*     } */
-
-/*     closedir(d); */
-/*   } */
-
-/*   return(0); */
-/* } */
-
-            int main(int argc, char ** argv)
-            {
-                char * path = argv[1];
-                brokensymlinks(path);
-                return 0;
-            }
+int main(int argc, char ** argv)
+{
+    char * path = argv[1];
+    printf("%s", path);
+    brokensymlinks(path);
+    return 0;
+}
