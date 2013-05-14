66ac1d89d575965c5ccdbdd416a11840eb0af592
jagajaga <senia00@gmail.com>
2013-05-15 03:30:37 +0400

    halfly fixed filter

--- filter/filter.c
+++ filter/filter.c
@@ -9,46 +9,57 @@ char new_line = '\n';
 size_t _write(int fd, char * buffer, size_t size)
 {
     size_t current = 0;
+
     while (current < size)
     {
         size_t result = write(fd, buffer + current, size);
+
         if (result < 1)
         {
             return current;
         }
+
         current += result;
         size -= result;
     }
+
     return current;
 }
 
 size_t _read(int fd, char * buffer, size_t size)
 {
     size_t current = 0;
+
     while (current < size)
     {
         size_t result = read(fd, buffer + current, size);
+
         if (result < 1)
         {
             return current;
         }
+
         current += result;
         size -= current;
     }
+
     return current;
 }
 
 int find_separator(char separator, char * buffer, int from, int size)
 {
     int pos = from;
+
     while (pos < size && buffer[pos] != separator)
     {
         pos++;
     }
+
     if (pos == size)
     {
         return -1;
     }
+
     return pos;
 }
 
@@ -56,18 +67,16 @@ void run_command(char ** command, char * buffer, int from, int size, char separa
 {
     int pid = fork();
     int child_status;
+
     if (pid)
     {
-        pid_t tpid;
-        do
+        wait(&child_status);
+
+        if (WIFEXITED(child_status) && !WEXITSTATUS(child_status))
         {
-            tpid = wait(&child_status);
-            if (WIFEXITED(child_status) && !WEXITSTATUS(child_status))
-            {
-                _write(1, buffer + from, size);
-                _write(1, &separator, 1);
-            }
-        } while (tpid != pid);
+            _write(1, buffer + from, size);
+            _write(1, &separator, 1);
+        }
     }
     else
     {
@@ -80,10 +89,9 @@ int main(int argc, char ** argv)
 {
     int opt;
     char * buffer;
-    char c, separator = new_line;
+    char separator = new_line;
     int buffer_size = 4096;
     int buffer_result;
-    int count = 0;
     char ** command;
 
     while ((opt = getopt(argc, argv, "nzb:")) != -1)
@@ -93,42 +101,55 @@ int main(int argc, char ** argv)
         case 'n':
             separator = new_line;
             break;
+
         case 'z':
             separator = zero;
             break;
+
         case 'b':
             buffer_size = atoi(optarg);
             break;
         }
     }
-    if (optind < argc) {
+
+    if (optind < argc)
+    {
         command = malloc((argc - optind + 2) * sizeof(char *));
         int i = 0;
+
         for (i = optind; i < argc; i++)
         {
             command[i - optind] = argv[i];
         }
+
         command[argc - optind + 1] = 0;
     }
+
     buffer = malloc(buffer_size);
+
     while (1)
     {
         int eof = 0;
         int count = 0;
+
         while(!eof)
         {
             if (count >= buffer_size)
             {
                 return -1;
             }
+			// это снова будет задерживать ввод
             buffer_result = _read(0, buffer + count, buffer_size - count);
+
             if (buffer_result == 0)
             {
                 eof = 1;
             }
+
             int from = count;
             count += buffer_result;
             int pos;
+
             while ((pos = find_separator(separator, buffer, from, count - from)) >= 0)
             {
                 char ** temp_command = command;
@@ -136,24 +157,29 @@ int main(int argc, char ** argv)
                 memcpy(temp_name, buffer, pos);
                 temp_command[argc - optind] = temp_name;
                 run_command(temp_command, buffer, 0, pos, separator);
-                memmove(buffer, buffer + (++pos), count - pos);
+                pos++;
+                memmove(buffer, buffer + pos, count - pos);
                 from = 0;
                 count -= pos;
             }
         }
+
         if (count > 0)
         {
             if (count + 1 >= buffer_size)
             {
                 return -1;
             }
+
             buffer[count + 1] = separator;
             char ** temp_command = command;
             temp_command[argc - optind + 1] = buffer;
             run_command(temp_command, buffer, 0, count + 1, separator);
         }
+
         break;
     }
+
     free(buffer);
     return 0;
 }
