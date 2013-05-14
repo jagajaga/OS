b37ff986760e3a6f2c2bc5f3b98d8305cae1d0ab
jagajaga <senia00@gmail.com>
2013-05-15 02:59:03 +0400

    improved and bugs fixed

--- midterm/streammerge.cpp
+++ midterm/streammerge.cpp
@@ -12,16 +12,20 @@
 size_t _write(int fd, char * buffer, size_t size)
 {
     size_t current = 0;
+
     while (current < size)
     {
         size_t result = write(fd, buffer + current, size);
+
         if (result < 1)
         {
             return current;
         }
+
         current += result;
         size -= result;
     }
+
     return current;
 }
 
@@ -37,6 +41,7 @@ int find_delimiter(char delimiter, char * buffer, int size)
             }
         }
     }
+
     return -1;
 }
 
@@ -50,12 +55,12 @@ private:
     static const int buffer_size = 4096;
     boost::optional<std::string> get_string_at_pos(int, int, int);
 public:
-    stream(int fd, char delimiter) : fd(fd), count(0), buffer((char*) malloc(buffer_size)), delimiter(delimiter){}
+    stream(int fd, char delimiter) : fd(fd), count(0), buffer((char *) malloc(buffer_size)), delimiter(delimiter) {}
     boost::optional<std::string> get_string();
     ~stream()
-        {
-            free(buffer);
-        }
+    {
+        free(buffer);
+    }
 };
 
 boost::optional<std::string> stream::get_string_at_pos(int pos, int from, int size)
@@ -65,42 +70,42 @@ boost::optional<std::string> stream::get_string_at_pos(int pos, int from, int si
         char * temp_name = (char *) malloc(pos);
         memcpy(temp_name, buffer, pos);
         pos++;
+
         if (count - pos < 0)
         {
             return boost::none;
         }
+
         memmove(buffer, buffer + pos, count - pos);
         from = 0;
         count -= pos;
         return std::string(temp_name);
     }
+
     return boost::none;
 }
 
 boost::optional<std::string> stream::get_string()
 {
-    char c;
     int buffer_result;
-    int eof = 0;
-    while(!eof)
+
+    while(1)
     {
         buffer_result = read(fd, buffer + count, buffer_size - count);
-        if (count >= buffer_size)
-        {
-            return boost::none;
-        }
         int from = count;
         count += buffer_result;
-        int pos;
+        int pos = 0;
+
         if (buffer_result == 0)
         {
             return get_string_at_pos(pos, from, count);
-            eof = 1;
         }
+
         if (buffer_result < 0)
         {
             break;
         }
+
         return get_string_at_pos(pos, from, count - from);
     }
     return boost::none;
@@ -118,13 +123,14 @@ public:
     boost::optional<std::string> get_string();
 
     ~streammerge()
-        {
-        };
+    {
+    };
 };
 
 boost::optional<std::string> streammerge::get_string()
 {
     current_fd = !current_fd;
+
     if (current_fd)
     {
         return s2.get_string();
@@ -133,6 +139,7 @@ boost::optional<std::string> streammerge::get_string()
     {
         return s1.get_string();
     }
+
     return boost::none;
 }
 
@@ -140,8 +147,24 @@ boost::optional<std::string> streammerge::get_string()
 
 int main(int argc, char ** argv)
 {
-    streammerge sm(open(argv[1], O_RDONLY), open(argv[2], O_RDONLY), argv[3][0]);
+    if (argc < 4)
+    {
+        std::cout << "Whrong argument size";
+        return 1;
+    }
+
+    int file1 = open(argv[1], O_RDONLY);
+    int file2 = open(argv[2], O_RDONLY);
+
+    if (file1 == -1 || file2 == -1)
+    {
+        std::cout << "Couldn't open file \"" << (file1 == 1 ? argv[1] : argv[2]) << "\"";
+		return 2;
+    }
+
+    streammerge sm(file1, file2, argv[3][0]);
     boost::optional<std::string> result;
+
     for (int i = 0; i < 10; i++)
         if (result = sm.get_string())
         {
